babd4977d4451ccaf9f2f3cfc2f49d6af347361f
intrening <wol87@ya.ru>
2019-06-09 15:39:25 +0300

    lesson02

--- server/products/views.py
+++ server/products/views.py
@@ -11,6 +11,7 @@ def product_list(request):
 def product_detail (request,pk):
     with open('server/products/fixtures/data.json') as file:
         context = json.load(file)
+        
     return render(request, 'products/detail.html', {'product':context[pk]})
 
 
