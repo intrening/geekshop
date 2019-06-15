0f74dcf9ae8da25057e7f39847242f07ee7ddbfc
intrening <wol87@ya.ru>
2019-06-15 21:20:29 +0300

    lesson 3

--- server/products/urls.py
+++ server/products/urls.py
@@ -7,5 +7,6 @@ app_name = 'products'
 urlpatterns = [
     path('', product_list, name='products'),
     path('<int:pk>/', product_detail, name='detail'),
+    path('category/<int:pk>/', category_detail, name='category_detail'),
 ]
 
--- server/products/views.py
+++ server/products/views.py
@@ -1,17 +1,31 @@
 from django.shortcuts import render
 import json
+from products.models import Product, ProductCategory
 
 # Create your views here.
 
 def product_list(request):
-    with open('server/products/fixtures/data.json') as file:
-        context = json.load(file)
-    return render(request, 'products/index.html', {'products':context})
+    products = Product.objects.all()
+    categorys = ProductCategory.objects.all()
+    return render(request, 'products/index.html', {
+        'products':products,
+        'categorys': categorys,
+        'title': 'Все продукты'
+        })
 
 def product_detail (request,pk):
-    with open('server/products/fixtures/data.json') as file:
-        context = json.load(file)
-        
-    return render(request, 'products/detail.html', {'product':context[pk]})
+    product = Product.objects.get(id=pk)
+    return render(request, 'products/detail.html', {
+        'product':product})
 
 
+def category_detail (request,pk):
+    #products = Product.objects.all()
+    #categorys = ProductCategory.objects.all()
+    category = ProductCategory.objects.get(id=pk)
+    products = Product.objects.filter(category=category)
+    return render(request, 'products/index.html', {
+        'products':products,
+        'categorys': [category],
+        'title': category.name
+        })
