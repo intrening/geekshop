62128a9b019e1ed6ebf776d51a80d3621b926681
intrening <wol87@ya.ru>
2019-09-03 15:46:46 +0300

    add abstract model

--- geekshop_project/products/models/Product.py
+++ geekshop_project/products/models/Product.py
@@ -1,8 +1,9 @@
 from django.db import models
+from . import ProductDate
 
 # Create your models here.
 
-class Product(models.Model):
+class Product(ProductDate, models.Model):
     category = models.ForeignKey('products.ProductCategory', on_delete=models.CASCADE)
     name = models.CharField(verbose_name='имя продукта', max_length=128, unique=True)
     image = models.ImageField(upload_to='products_images', blank=True)
@@ -10,13 +11,6 @@ class Product(models.Model):
     description = models.TextField(verbose_name='описание продукта', blank=True)
     price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='количество на складе', default=0)
-
-    modified = models.DateTimeField(
-        auto_now=True
-    )
-    created = models.DateTimeField(
-        auto_now_add=True
-    )
 
     def __str__(self):
         return f"{self.name} ({self.category.name})"
--- geekshop_project/products/models/ProductCategory.py
+++ geekshop_project/products/models/ProductCategory.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class ProductCategory(models.Model):
-    name = models.CharField(verbose_name='имя продукта', max_length=128, unique=True)
-    description = models.TextField(verbose_name='описание продукта', blank=True)
+class ProductDate(models.Model):
+    class Meta:
+        abstract = True
 
     modified = models.DateTimeField(
         auto_now=True
@@ -10,6 +10,10 @@ class ProductCategory(models.Model):
     created = models.DateTimeField(
         auto_now_add=True
     )
+
+class ProductCategory(ProductDate, models.Model):
+    name = models.CharField(verbose_name='имя продукта', max_length=128, unique=True)
+    description = models.TextField(verbose_name='описание продукта', blank=True)
 
     def __str__(self):
         return f"{self.name}"
